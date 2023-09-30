9bbf1fdc5f8c54cfd45d59a02a613d0977942698
jairojg <jj.ggonzalez.hernandez@gmail.com>
2023-09-30 18:47:46 -0500

    gitignore fixed and requirements added

--- app/app.py
+++ app/app.py
@@ -1,8 +1,14 @@
 from deepface import DeepFace
+import sqlite3
+import os
 
+#verification = DeepFace.verify(img1_path = "../data/test_img/Bono_0001.jpg", img2_path = "../data/test_img/Brad_Pitt_0001.jpg")
+#print(verification)
 
-recognition = DeepFace.verify(img1_path="./app/data/test_img/Bono_0001.jpg",
-                              img2_path="./app/data/test_img/Brad_Pitt_0001.jpg"
-                              )
+conn = sqlite3.connect('./data/facialdb.db')
+cursor = conn.cursor()
+
+#recognition = DeepFace.find(img_path= "../data/test_img/Bono_0001.jpg", db_path="~/jairojg/venv/data/facialdb.db")
+recognition = DeepFace.find(img_path= "./data/test_img/Bono_0001.jpg", db_path="./data/test_db",enforce_detection=False)
 print(recognition)
 
