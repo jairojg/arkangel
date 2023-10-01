ca4d842a16e1a42d2650e851e5ef55549a53e719
jairojg <jj.ggonzalez.hernandez@gmail.com>
2023-09-30 20:06:47 -0500

    feat: preparing for sentiment analysis and code cleaning performed

--- app/app.py
+++ app/app.py
@@ -5,69 +5,89 @@ import sqlite3
 import math
 
 # reading image and extracting embeddings
-target_img_path = 'data/test_img/Bono_0001.jpg'
-target_img = DeepFace.extract_faces(img_path = target_img_path)[0]["face"]
-target_embedding = DeepFace.represent(img_path = target_img_path, model_name = "Facenet")[0]["embedding"]
+# target_img_path = 'data/test_img/ai.jpg' # face not found on db
+# target_img_path = 'data/test_img/Blank_image.jpg' # face not found on image
+# target_img_path = 'data/test_img/Ezra_miller.jpg' # face on image causes false positive 
+target_img_path = 'data/test_img/group.jpg' # group 
+#target_img_path = 'data/test_img/aaron_eckhart.jpg'
 
 
-# database connection and data loading
-conn = sqlite3.connect('./data/facialdb.db')
-cursor = conn.cursor()
-
-######################
-
-target_statement = ''
-for i, value in enumerate(target_embedding):
-    target_statement += 'select %d as dimension, %s as value' % (i, str(value)) #sqlite
-     
-    if i < len(target_embedding) - 1:
-        target_statement += ' union all '
+try:
+    target_img = DeepFace.extract_faces(img_path = target_img_path)[0]["face"]
+    target_embedding = DeepFace.represent(img_path = target_img_path, model_name = "Facenet")[0]["embedding"]
 
 
-select_statement = f'''
-    select * 
-    from (
-        select img_name, sum(subtract_dims) as distance_squared, person_name
+    # database connection and data loading
+    conn = sqlite3.connect('./data/facialdb.db')
+    cursor = conn.cursor()
+
+    ######################
+
+    target_statement = ''
+    for i, value in enumerate(target_embedding):
+        target_statement += 'select %d as dimension, %s as value' % (i, str(value)) #sqlite
+        
+        if i < len(target_embedding) - 1:
+            target_statement += ' union all '
+
+
+    select_statement = f'''
+        select * 
         from (
-            select img_name, (source - target) * (source - target) as subtract_dims, person_name
+            select img_name, sum(subtract_dims) as distance_squared, person_name
             from (
-                select meta.img_name, emb.value as source, target.value as target, meta.person_name
-                from face_meta meta left join face_embeddings emb
-                on meta.id = emb.face_id
-                left join (
-                    {target_statement}  
-                ) target
-                on emb.dimension = target.dimension
+                select img_name, (source - target) * (source - target) as subtract_dims, person_name
+                from (
+                    select meta.img_name, emb.value as source, target.value as target, meta.person_name
+                    from face_meta meta left join face_embeddings emb
+                    on meta.id = emb.face_id
+                    left join (
+                        {target_statement}  
+                    ) target
+                    on emb.dimension = target.dimension
+                )
             )
+            group by img_name
         )
-        group by img_name
-    )
-    where distance_squared < 100
-    order by distance_squared asc
-'''
+        where distance_squared < 100
+        order by distance_squared asc
+    '''
 
 
-tic = time.time()
-results = cursor.execute(select_statement)
+    # Perform query
+    tic = time.time()
+    results = cursor.execute(select_statement)
+
+    instances = []
+    for result in results:
+        img_name = result[0]
+        distance_squared = result[1]
+        person_name = result[2]
+
+        instance = []
+        instance.append(img_name)
+        instance.append(math.sqrt(distance_squared))
+        instance.append(person_name)
+        instances.append(instance)
+
+    toc = time.time()
+    print("search finalizad in",toc-tic,"seconds")
+
+    result_df = pd.DataFrame(instances, columns = ["img_name", "distance", "person_name"])
+    
+    if not result_df.empty:
+        # if df is not empty there was a matching result
+        print(result_df["person_name"].head(1))
+
+    else:
+        # there was not result
+        print("person was not found on employees database")
+    
+    ## sentiment analysis
 
 
-print(results)
+except ValueError:
+    print("No face detected")
 
-instances = []
-for result in results:
-    img_name = result[0]
-    distance_squared = result[1]
-    person_name = result[2]
-
-    instance = []
-    instance.append(img_name)
-    instance.append(math.sqrt(distance_squared))
-    instance.append(person_name)
-    instances.append(instance)
-
-toc = time.time()
-print(toc-tic,"seconds")
-
-result_df = pd.DataFrame(instances, columns = ["img_name", "distance", "person_name"])
-
-print(result_df["person_name"].head(1))
+finally:
+    print("Closing app")
