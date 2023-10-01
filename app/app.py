06e7bbee21e2b65426e0815f290d623e298b2f41
jairojg <jj.ggonzalez.hernandez@gmail.com>
2023-09-30 19:20:43 -0500

    feat: easy face recognition solved

--- app/app.py
+++ app/app.py
@@ -1,14 +1,68 @@
 from deepface import DeepFace
+import pandas as pd
+import time
 import sqlite3
-import os
+import math
 
-#verification = DeepFace.verify(img1_path = "../data/test_img/Bono_0001.jpg", img2_path = "../data/test_img/Brad_Pitt_0001.jpg")
-#print(verification)
+# reading image and extracting embeddings
+target_img_path = 'data/test_img/Bono_0001.jpg'
+target_img = DeepFace.extract_faces(img_path = target_img_path)[0]["face"]
+target_embedding = DeepFace.represent(img_path = target_img_path, model_name = "Facenet")[0]["embedding"]
 
+
+# database connection and data loading
 conn = sqlite3.connect('./data/facialdb.db')
 cursor = conn.cursor()
 
-#recognition = DeepFace.find(img_path= "../data/test_img/Bono_0001.jpg", db_path="~/jairojg/venv/data/facialdb.db")
-recognition = DeepFace.find(img_path= "./data/test_img/Bono_0001.jpg", db_path="./data/test_db",enforce_detection=False)
-print(recognition)
+######################
 
+target_statement = ''
+for i, value in enumerate(target_embedding):
+    target_statement += 'select %d as dimension, %s as value' % (i, str(value)) #sqlite
+     
+    if i < len(target_embedding) - 1:
+        target_statement += ' union all '
+
+
+select_statement = f'''
+    select * 
+    from (
+        select img_name, sum(subtract_dims) as distance_squared
+        from (
+            select img_name, (source - target) * (source - target) as subtract_dims
+            from (
+                select meta.img_name, emb.value as source, target.value as target
+                from face_meta meta left join face_embeddings emb
+                on meta.id = emb.face_id
+                left join (
+                    {target_statement}  
+                ) target
+                on emb.dimension = target.dimension
+            )
+        )
+        group by img_name
+    )
+    where distance_squared < 100
+    order by distance_squared asc
+'''
+
+
+tic = time.time()
+results = cursor.execute(select_statement)
+
+instances = []
+for result in results:
+    img_name = result[0]
+    distance_squared = result[1]
+    
+    instance = []
+    instance.append(img_name)
+    instance.append(math.sqrt(distance_squared))
+    instances.append(instance)
+
+toc = time.time()
+print(toc-tic,"seconds")
+
+result_df = pd.DataFrame(instances, columns = ["img_name", "distance"])
+
+print(result_df.head(1))
