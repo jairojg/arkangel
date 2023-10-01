e48e08152866cd56fa62825ea7ed34e734385dd7
jairojg <jj.ggonzalez.hernandez@gmail.com>
2023-10-01 16:31:01 -0500

    final

--- networkapp/client_app.py
+++ networkapp/client_app.py
@@ -29,38 +29,12 @@ client_socket.connect((IP, PORT))
 #client_socket.settimeout(10)
 #client_socket.setblocking(False)
 
-incoming_data = []
-try:
-
-    print(f"connection stablished at {IP}:{PORT}")
+print(f"connection stablished at {IP}:{PORT}")
     
-    # load and send image
-    image = Image.open(IMG_PATH)
-    msg_out = pickle.dumps(image)
-    client_socket.send(msg_out)
-
-
-    while True:
-        ready_to_read, _, _ = select.select([client_socket], [], [], 0.1)  # Check for incoming data with a timeout
-
-        if client_socket in ready_to_read:
-            data = client_socket.recv(1024)  # Adjust the buffer size as needed
-
-            if not data:
-                break
-
-            incoming_data.append(data)
-
-        # You can perform other tasks here while waiting for a response
-
-    # Process the received data, if any
-    if incoming_data:
-        response = b''.join(incoming_data)
-        print("Received a response from the server:", response.decode('utf-8'))
-    else:
-        print("No response received from the server.")
-
-except Exception as e:
-    print("Error:", e)
-finally:
-    client_socket.close()
+# load and send image
+image = Image.open(IMG_PATH)
+msg_out = pickle.dumps(image)
+client_socket.send(msg_out)
+print("image sent to {IP}:{PORT}")
+client_socket.close()
+print("closing")
